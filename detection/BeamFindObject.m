f13da26b789772ef90bedd936c99b8831d4e12f6
Chao Li <jonathan.swjtu@gmail.com>
2016-11-06 20:54:13 +0800

    多目标扫描架构

---
+++ detection/BeamFindObject.m
@@ -0,0 +1,35 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Author: Chao Li                 %%%
+%%% Email: jonathan.swjtu@gmail.com %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [hasObject, objects_l, objects_w, objects_v] = BeamFindObject(beamPos_l, beamPos_w, beam_type)
+%判断波束所在方位有无目标，beamPos_l波束横向的位置，beamPos_w波束纵向的位置，beam_type波束的类型，1为大波束，2为小波束
+global map map_l map_w big_beam small_beam deltaR %引用全局变量
+if beam_type == 1
+    num_l = big_beam / map_l; %大波束内横向有多少个分辨单元
+    num_w = big_beam / map_w; %大波束内纵向有多少个分辨单元
+else
+    num_l = small_beam / map_l; %小波束内横向有多少个分辨单元
+    num_w = small_beam / map_w; %小波束内纵向有多少个分辨单元
+end
+objects_l = []; %单波束内所有物体的横坐标，每行代表一个物体
+objects_w = []; %单波束内所有物体的纵坐标，每行代表一个物体
+objects_v = []; %单波束内所有物体的速度，每行代表一个物体
+for i = 1:num_l
+    for j = 1:num_w
+        index_l = fix((beamPos_l - 1) * num_l + i);
+        index_w = fix((beamPos_w - 1) * num_w + j);
+        if(map(index_l, index_w) ~= 0)%TODO:改用回波处理算法，目前为了测试
+            hasObject = 1;
+            if beam_type == 1
+                objects_l = [objects_l;(beamPos_l - 1) *  big_beam + 0.5 * big_beam];
+            else
+                objects_l = [objects_l;(beamPos_l - 1) *  small_beam + 0.5 * small_beam];
+            end
+            objects_w = [objects_w; index_w * map_w];
+            objects_v = [objects_v; map(index_l, index_w)];
+        end
+    end
+end
+end
--- detection/radarDetection.m
+++ detection/radarDetection.m
@@ -1,11 +1,13 @@
-%%%%%%%%%%%%%%%%%%%%%%%
-%%% Author: Chao Li %%%
-%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Author: Chao Li                 %%%
+%%% Email: jonathan.swjtu@gmail.com %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 close all;
 clear all;
 clc;
 %全局变量设置
-global map VW RL RW map_l map_w map_length map_width R_pre_lmp R_pre_wmp objectNum;
+global map VW RL RW map_l map_w map_length map_width R_pre_lmp R_pre_wmp objectNum big_beam small_beam deltaR;
 load('simuConfig.mat');%导入雷达参数配置文件
 
 Tb = map_length*map_width/(big_beam*big_beam)*T1; %大波束扫描完整个区域的时间
@@ -13,8 +15,8 @@ t = 0:T1:time_num*Tb; %时间轴
 len = size(t,2);
 VL = V_init_l*ones(1,len) + A_init_l*t;%横向目标速度时间轴
 VW = V_init_w*ones(1,len) + A_init_w*t;%纵向目标速度时间轴
-RL = R_init_l*ones(1,len) + (V_init_l*t + 0.5*A_init_l*t.^2);
-RW = R_init_w*ones(1,len) + (V_init_w*t + 0.5*A_init_w*t.^2);
+RL = R_init_l*ones(1,len) + (V_init_l*t + 0.5*A_init_l*t.^2);%横向目标距离时间轴
+RW = R_init_w*ones(1,len) + (V_init_w*t + 0.5*A_init_w*t.^2);%纵向目标距离时间轴
 %TODO:加速度可以在一定范围内浮动
 
 
@@ -24,21 +26,41 @@ global outOfRange;
 outOfRange = zeros(objectNum, 1);
 prePath = cell(1,objectNum);
 %波束相关参数
+beamPos_w = 1;
+beamPos_l = 1;%波束的位置
 big_has_small_num = ceil(big_beam/small_beam); %大波束内包含的小波束个数
 num_l = map_length / big_beam; %大波束横轴扫描次数
 num_w = map_width / big_beam;%大波束纵轴扫描次数
+track_flag = 0; %判断是否进入跟踪模式，0为不跟踪，扫描模式，1为大波束跟踪，2为小波束跟踪
+points = []; %保存扫描模式下扫描完整个区域的点迹的一个周期的点迹，分为多行，每行分别为[横向距离 纵向距离 纵向速度]
 for i = 1:len
     updatemap(i); %实时更新map
     for index = 1:objectNum
-       if outOfRange(index) == 0
-           prePath{1,index} = [prePath{1,index} [R_pre_lmp(index)*map_l; R_pre_wmp(index)*map_w]];
-       end
+        if outOfRange(index) == 0
+            prePath{1,index} = [prePath{1,index} [R_pre_lmp(index)*map_l; R_pre_wmp(index)*map_w]]; %模拟的理论运动模型轨迹
+        end
     end
+    if track_flag == 0 %扫描模式
+        [hasObject, objects_l, objects_w, objects_v] = BeamFindObject(beamPos_l, beamPos_w, 1);%判断当前波束是否有目标，如果有，记录下点迹
+        if hasObject
+            points = [points ;objects_l objects_w objects_v];
+        end
+    end
+    beamPos_l = beamPos_l + 1;%切换下一个波束，先横着扫，当扫到行尾时切换到下一行
+    if beamPos_l > num_l
+        beamPos_w = beamPos_w + 1; %换行
+        beamPos_l = 1;%切换到行首
+    end
+    
+    if beamPos_w > num_w
+        %一个整周期扫描结束，开始后续的分析处理
+        beamPos_w = 1;
+        beamPos_l = 1;
 end
 %测试运动模型轨迹路径
-% figure
-% for index = 1:objectNum
-%     plot(prePath{1,index}(1,:),prePath{1,index}(2,:),'*');
-%     hold on
-% end
+figure
+for index = 1:objectNum
+    plot(prePath{1,index}(1,:),prePath{1,index}(2,:),'*');
+    hold on
+end
 
